4c2dc4e1318435e190c10935a5ace4e3f6792927
jpummill <jpummill@yahoo.com>
2022-03-12 09:26:48 -0600

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "s3-billing-02" {
+    bucket = "terraform-s3-billing-02"
+    tags = {
+        creator = "jpummill"
+        purpose = "Terraform testing"
+    }
+}
