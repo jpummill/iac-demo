e63d106f0ffb2cdf004000a256c167e26f7aa5ed
jpummill <jpummill@yahoo.com>
2022-03-13 10:45:48 -0500

    Update s3b2.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- s3b2.tf
+++ s3b2.tf
@@ -4,6 +4,13 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "s3-billing-03" {
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
     bucket = "terraform-s3-billing-03"
     tags = {
         creator = "jpummill"
